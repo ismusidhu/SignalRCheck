b6d6d5bb0cdc00ea4d81cab490cdf1e499e4f822
ismusidhu <ismailsidhpuri@gmail.com>
2023-08-29 18:10:52 +0530

    init

---
+++ SignalRCheck/Data/TapDeviceHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+public class TapDeviceHub : Hub
+{
+    public const string HubUrl = "/scanning";
+
+    public override Task OnConnectedAsync()
+    {
+        Console.WriteLine($"#########################{Context.ConnectionId}connected");
+        return base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception e)
+    {
+        await base.OnDisconnectedAsync(e);
+    }
+
+    public async Task OnTap(string deviceName, string userID)
+    {
+        string connectionID = "sdfsaf"; //getconnectin ID
+        await Clients.Client(connectionID).SendAsync("OnTap", deviceName, "userID");
+    }
+}
